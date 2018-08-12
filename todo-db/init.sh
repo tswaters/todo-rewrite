4c9f6a8684bd7ff343126507cac052831f141eee
Tyler Waters <tyler.waters@gmail.com>
2018-08-12 08:42:36 -0700

    pgcrypto was added to wrong db

--- todo-db/init.sh
+++ todo-db/init.sh
@@ -7,6 +7,10 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
   CREATE DATABASE todo;
   GRANT ALL PRIVILEGES ON DATABASE todo TO todo;
 
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname todo <<-EOSQL
+
   CREATE EXTENSION pgcrypto;
 
 EOSQL
