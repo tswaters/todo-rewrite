4f76f6b524353de54eeaab6900742d37f0e4c851
Tyler Waters <tyler.waters@gmail.com>
2018-09-15 21:55:00 -0700

    Fix ./tests.sh to properly wait for healthy dependent services

--- tests.sh
+++ tests.sh
@@ -1,13 +1,20 @@
+#!/bin/bash
+set -e
 
 # spin up the dependencies & wait for things to be fine.
 # ddl is a good for `postgres` but we need to wait for amqp/redis as well.
 
-docker-compose -p todo-test up --build -d
+docker-compose -p todo-test -f docker-compose.yml -f docker-compose-dev.yml -f docker-compose-build.yml up --build -d
 docker wait todo-test_ddl_1
 docker run --rm --network todo-test_default -e TARGETS=rabbitmq:5672,postgres:5432,redis:6379 waisbrot/wait
 
 # pull out .env/* into environment variables for the test process
 # a few overrides are needed to to account for different project name
+
+services=(todo-items todo-auth todo-localization)
+for f in ${services[@]}; do
+  docker run --network todo-test_default --rm -e URL=$f:49999/health stefanevinance/wait-for-200
+done
 
 for f in .env/*; do
   export $(basename $f)=$(cat $f)
